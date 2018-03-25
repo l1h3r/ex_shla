eebbfcd5e52fe9934ef4f2aa746f400ceef75a78
l1h3r <devin.turner09@gmail.com>
2018-03-25 11:54:48 -0700

    initial commit

---
+++ lib/ex_shla.ex
@@ -0,0 +1,18 @@
+defmodule ExShla do
+  @moduledoc """
+  Documentation for ExShla.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExShla.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_shla_test.exs
@@ -0,0 +1,8 @@
+defmodule ExShlaTest do
+  use ExUnit.Case
+  doctest ExShla
+
+  test "greets the world" do
+    assert ExShla.hello() == :world
+  end
+end
